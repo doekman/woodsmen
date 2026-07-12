453f5cd8a9a946b5ea584f0656fab5caaa11e348
Doeke Zanstra <doekman@icloud.com>
2026-07-12 12:26:18 +0200

    Horizontal scroll on small screens

--- docs/2026/index/the.js
+++ docs/2026/index/the.js
@@ -22,32 +22,43 @@ new function() { // closure
         carousel.style.transform = 'translateX(' + (-currentIndex * 100) + '%)';
       }
     }
+    
+    function initializeHorizontalShift() {
+      // Not yet generic!
+      const matches = document.querySelectorAll('.horizontal-shift');
+      let horizontalShiftElements = []
+      matches.forEach( (img) => {
+        const imgCurrentStyle = window.getComputedStyle(img);
+        if (imgCurrentStyle.objectFit == 'cover') {
+          horizontalShiftElements.push(img);
+          //console.log("Image registered for horizontal shifting", window.innerHeight);
+        }
+      });
+      const maxScrollY = 510;
+      const objectPositionStartEnd = [10, 65];
+      addEventListener("scroll", (event) => {
+        for(const element of horizontalShiftElements) {
+          let scrollPercentage = window.scrollY / maxScrollY;
+          let objectPositionPct = objectPositionStartEnd[1];
+          if (window.scrollY < maxScrollY) {
+            objectPositionPct = ((objectPositionStartEnd[1] - objectPositionStartEnd[0]) * scrollPercentage) + objectPositionStartEnd[0];
+          }
+          else {
+            objectPositionPct = objectPositionStartEnd[1]
+          }
+          //console.info("horizontal %s -- %s", scrollPercentage, objectPositionPct);
+          element.style.objectPosition = `${objectPositionPct}%`;
+        }
+      });
+    }
 
   document.addEventListener("DOMContentLoaded", (event) => {
-    /*
-    const matches = document.querySelectorAll('.horizontal-shift');
-    let horizontalShiftElements = []
-    matches.forEach( (img) => {
-      const imgCurrentStyle = window.getComputedStyle(img);
-      if (imgCurrentStyle.objectFit == 'cover') {
-        horizontalShiftElements.push(img);
-        console.log("Image registered for horizontal shifting", window.innerHeight);
-      }
-    });
-    addEventListener("scroll", (event) => {
-      for(const element of horizontalShiftElements) {
-        const bounds = element.getBoundingClientRect();
-        const xxx = bounds.bottom - window.innerHeight;
-        if (xxx > 0) console.info("horizontal %s", xxx, bounds);
-        else console.info('Nothing to do')
-      }
-    });
-    */
+    initializeHorizontalShift();
     initializeCarousel();
     console.log("%c _   _           __        __              _                          \n" +
                 "| |_| |__   ___  \\ \\      / /__   ___   __| |___ _ __ ___   ___ _ __  \n" +
                 "| __| '_ \\ / _ \\  \\ \\ /\\ / / _ \\ / _ \\ / _` / __| '_ ` _ \\ / _ \\ '_ \\ \n" +
                 "| |_| | | |  __/   \\ V  V / (_) | (_) | (_| \\__ \\ | | | | |  __/ | | |\n" +
-                " \\__|_| |_|\\___|    \\_/\\_/ \\___/ \\___/ \\__,_|___/_| |_| |_|\\___|_| |_|", 'font-family: monospace');
+                " \\__|_| |_|\\___|    \\_/\\_/ \\___/ \\___/ \\__,_|___/_| |_| |_|\\___|_| |_|", 'font-family: monospace; text-wrap:nowrap');
   });
 }
